3ba057cde10a9ff33e65cf452f214684cd4c9688
Mark Ghaida <markghaida@gmail.com>
2021-01-31 18:31:33 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+// import UserInfo from "./data/user"
 function App() {
   return (
     <div>
